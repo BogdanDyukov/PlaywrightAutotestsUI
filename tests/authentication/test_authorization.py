3ede1a03b7c5aacc8d5828ed3603b76a0597fa9c
BogdanDyukov <informatochka@mail.ru>
2025-08-06 11:08:20 +0700

    Generation allure in another job and Add error specially

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -35,6 +35,7 @@ class TestAuthorization:
         login_page.login_form.fill(email, password)
         login_page.click_login_button()
         login_page.check_visible_wrong_email_or_password_alert()
+        assert False
 
     @allure.tag(AllureTag.USER_LOGIN)
     @allure.title("User login with correct email and password")
